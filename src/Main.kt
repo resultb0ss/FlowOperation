6afbf16f67b6f4b0cb1b7c4c2b5e5afd52aae622
Admin <reteamboss@gmail.com>
2024-10-02 10:59:36 +0300

    1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
-fun main() {
-    println("Hello World!")
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.reduce
+import kotlin.system.measureTimeMillis
+
+suspend fun main() {
+
+    val time = measureTimeMillis {
+        val numArray = arrayOf(1,2,3,4,5,6,7,8,9,10).asFlow().map { it->it*it }.reduce{a,b -> a + b}
+        println(numArray)
+
+    }
+    println("Времени затрачено $time")
+
 }
+
